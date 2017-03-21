3b36e65f2a11a5ba9d6c5412380da95b4421ca85
othree <othree@gmail.com>
2017-03-21 18:40:55 +0800

    Support *.ts, *.tsx on cli

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
+var minimist = require('minimist')
+
+var argv = minimist(process.argv.slice(2));
+
+if (!argv._.length) {
+  process.argv.push(
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.ts',
+    '**/*.tsx'
+  );
+}
 
 var opts = require('../options.js')
 require('standard-engine').cli(opts)
