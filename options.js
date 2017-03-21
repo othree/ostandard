3cbd9d64f482880742fea2000a3789a01539736d
othree <othree@gmail.com>
2017-03-21 11:49:26 +0800

    update rule

--- options.js
+++ options.js
@@ -7,7 +7,7 @@ module.exports = {
   version: pkg.version,
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
-  tagline: 'Semicolons For All!',
+  tagline: 'Airbnb For All!',
   eslint: require('eslint'),
   eslintConfig: {
     configFile: path.join(__dirname, 'eslintrc.json')
